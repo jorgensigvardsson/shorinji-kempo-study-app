08b7f5309927d4df7f49cce3a7b62b6513335199
Jörgen Sigvardsson <jorgen.sigvardsson@cash-it.se>
2026-04-18 19:07:35 +0200

    SASS nu

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode, type ReactNode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/bootstrap-theme.scss';
 import App from './App.tsx'
 import { BrowserRouter } from "react-router-dom";
 import gradePlans from './assets/kamokuhyo.json';
