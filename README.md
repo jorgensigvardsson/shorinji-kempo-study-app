d6fb68467d82244e5b902fc88d63c61d9e367159
Jörgen Sigvardsson <jorgen.sigvardsson@cash-it.se>
2026-04-20 09:42:38 +0200

    Fixade till margin

--- README.md
+++
@@ -1,9 +0,0 @@
-# Data som saknas
-## Foot stance
-HokeiMoment.footStance: string[], t.ex. ["tai gamae", "hiraki gamae"]
-
-## Ren hanko
-HokeiMoment.roles.defender.renHanko: boolean
-
-## Kihon shoho
-Inga översättningar gjorda för kihon shoho
--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,7 @@ function App(props: Props) {
     <TranslatorContext.Provider value={translator}>
       <div style={{ zoom: textZoom }}>
         <AppNavbar routes={routes} translator={translator} className="d-print-none"/>
-        <div className="m-3">
+        <div className="m-1">
           {renderRoutes(routes)}
           <Outlet />
         </div>
