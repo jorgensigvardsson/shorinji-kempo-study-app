751c1e4b2143488d56bf0c3534adcd0c5fe8675e
Jörgen Sigvardsson <jorgen.sigvardsson@cash-it.se>
2026-04-27 21:06:45 +0200

    testing

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-7a5e81cd'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.0hlrs7g4tao"
+    "revision": "0.e3efq33uldc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
