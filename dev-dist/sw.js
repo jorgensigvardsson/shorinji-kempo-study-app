3c0d99a234dd5feeeb5364b5c47e3cb536dda875
Jörgen Sigvardsson <jorgen.sigvardsson@cash-it.se>
2026-05-03 19:05:17 +0200

    Added missing translations

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-7a5e81cd'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.mhn9mtr51fk"
+    "revision": "0.q4h2e0ell48"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
